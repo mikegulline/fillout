c8eda546e4f3b186c2d86c85aa6a1b9a499fa189
Mike Gulline <mike@mikegulline.com>
2025-06-16 21:21:09 -0700

    add new step

---
+++ src/components/ui/button-add.tsx
@@ -0,0 +1,25 @@
+import PlusIcon from '@/assets/icon-plus-small.svg';
+
+export const ButtonAdd = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <button onClick={onClick}>
+      <div className='group cursor-pointer flex items-center justify-center w-5 hover:w-14 transition-[width] duration-200 delay-400'>
+        <div className='relative h-4 w-full flex items-center justify-center'>
+          <div className='w-full h-0 border-t-[1.5px] border-[#C0C0C0] border-dashed' />
+          <div
+            className='
+      opacity-0 scale-0
+      group-hover:opacity-100 group-hover:scale-100
+      transition-all duration-500 delay-300
+      absolute w-4 h-4 bg-white border-[0.5px] border-[#E1E1E1]
+      rounded-full shadow-[0px_1px_3px_#0000000A,0px_1px_1px_#00000005]
+      flex justify-center items-center
+    '
+          >
+            <PlusIcon />
+          </div>
+        </div>
+      </div>
+    </button>
+  );
+};
--- src/components/ui/step-navigation.tsx
+++ src/components/ui/step-navigation.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState } from 'react';
-import { Button, Buttons } from '@/components/ui/button';
+import { Button, Buttons, ButtonIcons } from '@/components/ui/button';
+import { ButtonAdd } from './button-add';
 
 type StepNavigationProps = {
   buttons: Buttons;
@@ -8,11 +9,33 @@ type StepNavigationProps = {
 
 export const StepNavigation = ({ buttons }: StepNavigationProps) => {
   const [navigation, setNavigation] = useState(buttons);
+
+  const handleAddStep = (index: number) => {
+    setNavigation((prev) => {
+      const newElement = {
+        name: 'New Step',
+        icon: 'info' as ButtonIcons,
+      };
+
+      const before = prev.slice(0, index);
+      const after = prev.slice(index);
+
+      return [...before, newElement, ...after];
+    });
+  };
+
   return (
     <div className='w-[1140px] h-[72px] p-5'>
-      <ul className='w-[1100px] h-8 justify-start gap-3 rounded-[12px] flex'>
-        {navigation?.map(({ name, icon }) => (
-          <li key={name}>
+      <ul className='w-[1100px] h-8 justify-start rounded-[12px] flex'>
+        {navigation?.map(({ name, icon }, i) => (
+          <li key={name + i} className='flex'>
+            {i > 0 && (
+              <ButtonAdd
+                onClick={() => {
+                  handleAddStep(i);
+                }}
+              />
+            )}
             <Button name={name} icon={icon} />
           </li>
         ))}

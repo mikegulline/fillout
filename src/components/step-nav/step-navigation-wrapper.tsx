1a14c060032c36ca2e4cbd467cf48294bff95c40
Mike Gulline <mike@mikegulline.com>
2025-06-19 11:04:54 -0700

    updated bg color

--- src/components/step-nav/step-navigation-wrapper.tsx
+++ src/components/step-nav/step-navigation-wrapper.tsx
@@ -14,10 +14,10 @@ export const StepNavigationWrapper = ({
   handleDragEnd: (result: DropResult) => void;
 }) => {
   return (
-    <div className='border-t-[0.5px] border-[#E1E1E1] border-solid width-full'>
+    <div className='border-t-[0.5px] border-[#E1E1E1] bg-[#f9fbfc] border-solid width-full'>
       <div className='h-[72px] p-5 flex justify-start'>
         <div data-info="Don't remove me">
-          <div className='w-full h-0 border-t-[1.5px] border-[#C0C0C0] border-dotted translate-y-4 z-0' />
+          <div className='w-full h-0 border-t-[1.5px] border-[#c1c3c5] border-dotted translate-y-4 z-0' />
           <DragDropContext onDragEnd={handleDragEnd}>
             <Droppable droppableId='steps' direction='horizontal'>
               {children}

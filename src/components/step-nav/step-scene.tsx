beb7d1ac743431bef161700007b610166794cc82
Mike Gulline <mike@mikegulline.com>
2025-06-19 10:44:56 -0700

    updated infos about task

--- src/components/step-nav/step-scene.tsx
+++ src/components/step-nav/step-scene.tsx
@@ -9,14 +9,16 @@ export const StepScene = () => {
   return (
     <div className='flex-grow gap-4 flex flex-col justify-center items-center w-full color text-[#677289] text-center'>
       <h1 className='text-5xl font-bold'>Current Page: {active?.name}</h1>
-      <p>(Mockup to recreate.)</p>
+      <p>(Image of mockup to recreate. Final ui at the bottom of the page.)</p>
       <Image src={Mockup} alt='Mockup' width={1224} height={526} />
       <p>
+        (Click to activate and show three dots.)
+        <br />
         (Righ click on the three dots to open settings menu.)
         <br />
         (Click and hold the three dots to drag and reorder buttons.)
         <br />
-        (Hover between buttons to show add page button.)
+        (Hover between buttons to show "add page" button.)
       </p>
     </div>
   );
